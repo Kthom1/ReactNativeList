239bd39e058ce97f81922fa775ea1354c0c96587
Kthom1 <keenan.thompson67@gmail.com>
2019-09-26 15:50:52 +0800

    Can add items to the list; Added basic styles

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState("");
+  const [listItems, setListItems] = useState([]);
+  const textInputHandler = enteredText => {
+    setEnteredText(enteredText);
+  };
+  const addTextHander = () => {
+    setListItems(listItems => [...listItems, enteredText]);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open</Text>
+      <View style={styles.firstInner}>
+        <TextInput
+          placeholder="Add Something"
+          style={styles.textInput}
+          onChangeText={textInputHandler}
+        />
+        <Button title="ADD" onPress={addTextHander} />
+      </View>
+      <View>
+        {listItems.map((item, index) => (
+          <View key={index} style={styles.listItem}>
+            <Text>{item}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    padding: 50
+  },
+  firstInner: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%"
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1
   }
 });
